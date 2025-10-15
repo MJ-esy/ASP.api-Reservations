6b7a1986fd75918e512e4ee931bde6c47c8efedc
MJ Eng <mj.eng.contact@gmail.com>
2025-10-15 19:53:59 +0200

    Refactor seeding and formatting in AppDbContext
    Refactored `AppDbContext` and `SeedData` for improved readability and maintainability. Added a new `SeedAdmin` method to seed default admin data. Reformatted Fluent API configurations and seeding methods for consistency. No functional changes to existing seed data.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,79 +3,80 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ASP_Reservations.Data
 {
-    public class AppDbContext : DbContext
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Table> Tables { get; set; }
+    public DbSet<Booking> Booking { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+
+
+    //Fluent API configurations
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        { }
+      base.OnModelCreating(modelBuilder);
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Table> Tables { get; set; }
-        public DbSet<Booking> Booking { get; set; }
-        public DbSet<Dish> Dishes { get; set; }
-        public DbSet<Admin> Admins { get; set; }
+      //User
+      var userEntity = modelBuilder.Entity<User>();
+      userEntity.Property(u => u.Name).IsRequired();
+      userEntity.HasIndex(u => new { u.Email, u.Phone }).IsUnique();
 
+      //Table
+      var tableEntity = modelBuilder.Entity<Table>();
+      tableEntity.HasIndex(t => t.Capacity);
 
-        //Fluent API configurations
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+      //Booking
+      var bookingEntity = modelBuilder.Entity<Booking>();
+      bookingEntity.Property(b => b.GuestNum).HasDefaultValue(1)
+          .IsRequired();
+      bookingEntity.HasIndex(b => new { b.Status, b.TableIdFk, b.StartDateTime })
+          .HasFilter("[Status] != 'Cancelled'");
+      bookingEntity.Property(e => e.Status)
+             .IsRequired()
+             .HasConversion<string>()
+             .HasMaxLength(20);
+      bookingEntity.Property(e => e.CreatedAt)
+              .IsRequired()
+              .HasDefaultValueSql("GETUTCDATE()");
+      bookingEntity.HasOne(e => e.User)
+             .WithMany(c => c.Bookings)
+             .HasForeignKey(e => e.UserIdFk)
+             .OnDelete(DeleteBehavior.Restrict)
+             .HasConstraintName("FK_Bookings_Customers");
+      bookingEntity.HasOne(e => e.Table)
+               .WithMany(c => c.Bookings)
+               .HasForeignKey(e => e.TableIdFk)
+               .OnDelete(DeleteBehavior.Restrict)
+               .HasConstraintName("FK_Bookings_Tables");
 
-            //User
-            var userEntity = modelBuilder.Entity<User>();
-            userEntity.Property(u => u.Name).IsRequired();
-            userEntity.HasIndex(u => new { u.Email, u.Phone }).IsUnique();
+      //Dish
+      var dishEntity = modelBuilder.Entity<Dish>();
+      dishEntity.Property(d => d.DishName).IsRequired();
+      dishEntity.Property(d => d.IsPopular).HasDefaultValue(false);
+      dishEntity.HasIndex(d => d.IsPopular);
+      dishEntity.Property(d => d.Allergen).IsRequired();
 
-            //Table
-            var tableEntity = modelBuilder.Entity<Table>();
-            tableEntity.HasIndex(t => t.Capacity);
+      //Admin
+      var adminEntity = modelBuilder.Entity<Admin>();
+      adminEntity.HasIndex(a => a.Username).IsUnique();
+      adminEntity.Property(a => a.Username).IsRequired();
+      adminEntity.Property(a => a.PasswordHash).IsRequired()
+          .HasMaxLength(100);
+      adminEntity.Property(a => a.Role).IsRequired()
+          .HasMaxLength(15)
+          .HasDefaultValue("ADMIN");
 
-            //Booking
-            var bookingEntity = modelBuilder.Entity<Booking>();
-            bookingEntity.Property(b => b.GuestNum).HasDefaultValue(1)
-                .IsRequired();
-            bookingEntity.HasIndex(b => new { b.Status, b.TableIdFk, b.StartDateTime })
-                .HasFilter("[Status] != 'Cancelled'");
-            bookingEntity.Property(e => e.Status)
-                   .IsRequired()
-                   .HasConversion<string>()
-                   .HasMaxLength(20);
-            bookingEntity.Property(e => e.CreatedAt)
-                    .IsRequired()
-                    .HasDefaultValueSql("GETUTCDATE()");
-            bookingEntity.HasOne(e => e.User)
-                   .WithMany(c => c.Bookings)
-                   .HasForeignKey(e => e.UserIdFk)
-                   .OnDelete(DeleteBehavior.Restrict)
-                   .HasConstraintName("FK_Bookings_Customers");
-            bookingEntity.HasOne(e => e.Table)
-                     .WithMany(c => c.Bookings)
-                     .HasForeignKey(e => e.TableIdFk)
-                     .OnDelete(DeleteBehavior.Restrict)
-                     .HasConstraintName("FK_Bookings_Tables");
-
-            //Dish
-            var dishEntity = modelBuilder.Entity<Dish>();
-            dishEntity.Property(d => d.DishName).IsRequired();
-            dishEntity.Property(d => d.IsPopular).HasDefaultValue(false);
-            dishEntity.HasIndex(d => d.IsPopular);
-            dishEntity.Property(d => d.Allergen).IsRequired();
-
-            //Admin
-            var adminEntity = modelBuilder.Entity<Admin>();
-            adminEntity.HasIndex(a => a.Username).IsUnique();
-            adminEntity.Property(a => a.Username).IsRequired();
-            adminEntity.Property(a => a.PasswordHash).IsRequired()
-                .HasMaxLength(100);
-            adminEntity.Property(a => a.Role).IsRequired()
-                .HasMaxLength(15)
-                .HasDefaultValue("ADMIN");
-
-            //Data Seeding
-            modelBuilder.SeedDishes();
-            modelBuilder.SeedUsers();
-            modelBuilder.SeedTables();
-        }
-
-
+      //Data Seeding
+      modelBuilder.SeedAdmin();
+      modelBuilder.SeedDishes();
+      modelBuilder.SeedUsers();
+      modelBuilder.SeedTables();
     }
+
+
+  }
 }
--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -4,168 +4,174 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ASP_Reservations.Data
 {
-    public static class SeedData
+  public static class SeedData
+  {
+    public static void SeedAdmin(this ModelBuilder modelBuilder)
     {
-        public static void SeedUsers(this ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>().HasData(
-                new User { UserId = 1, Name = "Test One", Email = "One@email.com", Phone = "123-456-7890" },
-                new User { UserId = 2, Name = "Test Two", Email = "Two@email.com", Phone = "234-567-8901" },
-                new User { UserId = 3, Name = "Test Three", Email = "Three@email.com", Phone = "345-678-9012" }
-            );
-        }
-        public static void SeedTables(this ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Table>().HasData(
-                new Table { TableId = 1, TableNum = 101, Capacity = 2 },
-                new Table { TableId = 2, TableNum = 102, Capacity = 2 },
-                new Table { TableId = 3, TableNum = 201, Capacity = 4 },
-                new Table { TableId = 4, TableNum = 202, Capacity = 4 },
-                new Table { TableId = 5, TableNum = 203, Capacity = 4 },
-                new Table { TableId = 6, TableNum = 204, Capacity = 4 },
-                new Table { TableId = 7, TableNum = 301, Capacity = 8 }
-            );
-        }
-
-        public static void SeedDishes(this ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Dish>().HasData(
-                new Dish
-                {
-                    DishId = 1,
-                    DishName = "Skagen Sandwich",
-                    Price = 175,
-                    Description = "sourdough bread, pickled cucumber trout roe(E,G,L)",
-                    IsPopular = true,
-                    Category = DishCategories.Starters,
-                    Allergen = Allergy.Gluten | Allergy.Eggs | Allergy.Lactose,
-                    ImageUrl = "https://shared.cdn.smp.schibsted.com/v2/images/4c0c27ff-0d8d-4616-91a4-5ade5020e187?fit=crop&h=1426&w=1900&s=c946556f4ea073d8d4bcbd9f68761e90c8be64f0"
-                },
-                new Dish
-                {
-                    DishId = 2,
-                    DishName = "Skagen Sandwich",
-                    Price = 245,
-                    Description = "sourdough bread, pickled cucumber trout roe (E,G,L)",
-                    IsPopular = false,
-                    Category = DishCategories.Starters,
-                    Allergen = Allergy.Gluten | Allergy.Eggs | Allergy.Lactose
-                },
-                new Dish
-                {
-                    DishId = 3,
-                    DishName = "Chantarelle toast",
-                    Price = 160,
-                    Description = "parmesan grilled sour dough (G,L)",
-                    IsPopular = true,
-                    Category = DishCategories.Starters,
-                    Allergen = Allergy.Gluten | Allergy.Lactose,
-                    ImageUrl = "https://i.ytimg.com/vi/-cK2rs86oSQ/hqdefault.jpg"
-                },
-                new Dish
-                {
-                    DishId = 4,
-                    DishName = "Smoked Tiger shrimps",
-                    Price = 175,
-                    Description = "pickled red onions, new potatoes (L)",
-                    IsPopular = false,
-                    Category = DishCategories.Starters,
-                    Allergen = Allergy.Lactose
-                },
-                new Dish
-                {
-                    DishId = 5,
-                    DishName = "Steak Tartare",
-                    Price = 185,
-                    Description = "angel fries, parmesan, grilled heart salad",
-                    IsPopular = false,
-                    Category = DishCategories.Starters
-                },
-                new Dish
-                {
-                    DishId = 6,
-                    DishName = "Nicoise",
-                    Price = 275,
-                    Description = "tuna, egg, capers",
-                    IsPopular = false,
-                    Category = DishCategories.Salad,
-                    Allergen = Allergy.Eggs
-                },
-                new Dish
-                {
-                    DishId = 7,
-                    DishName = "Moules frites",
-                    Price = 285,
-                    Description = "aioli (E,L)",
-                    IsPopular = false,
-                    Category = DishCategories.Mains,
-                    Allergen = Allergy.Lactose | Allergy.Eggs
-                },
-                  new Dish
-                  {
-                      DishId = 8,
-                      DishName = "Baked Trout",
-                      Price = 295,
-                      Description = "Sandefjord sauce, trout roe, Chantarelles, boiled potatoes (L)",
-                      IsPopular = true,
-                      Category = DishCategories.Mains,
-                      Allergen = Allergy.Lactose,
-                      ImageUrl = "https://www.livinglou.com/wp-content/uploads/2010/08/oven-baked-trout-fillets.jpg"
-                  },
-                  new Dish
-                  {
-                      DishId = 9,
-                      DishName = "Ossobuco",
-                      Price = 295,
-                      Description = "veal, crushed potatoes, red wine sauce, creamy gremolata (E,G,L)",
-                      IsPopular = false,
-                      Category = DishCategories.Mains,
-                      Allergen = Allergy.Lactose | Allergy.Gluten | Allergy.Eggs
-                  },
-                  new Dish
-                  {
-                      DishId = 10,
-                      DishName = "Pepper Steak",
-                      Price = 395,
-                      Description = "green peppar sauce, French fries, parmesan (L)",
-                      IsPopular = true,
-                      Category = DishCategories.Mains,
-                      Allergen = Allergy.Lactose,
-                      ImageUrl = "https://embed.widencdn.net/img/beef/mmnbci53nd/1540x1284px/t-bone-steaks-&-grilled-vegetables-vertical.eps?keep=c"
-                  },
-                  new Dish
-                  {
-                      DishId = 11,
-                      DishName = "Apple pie",
-                      Price = 130,
-                      Description = "vanilla ice cream (E,L)",
-                      IsPopular = true,
-                      Category = DishCategories.Dessert,
-                      Allergen = Allergy.Lactose | Allergy.Gluten | Allergy.Eggs,
-                      ImageUrl = "https://onedishkitchen.com/wp-content/uploads/2018/10/apple-pie-one-dish-kitchen-1-1.jpg"
-                  },
-                  new Dish
-                  {
-                      DishId = 12,
-                      DishName = "Creme brulee",
-                      Price = 110,
-                      Description = "(E,L)",
-                      IsPopular = false,
-                      Category = DishCategories.Dessert,
-                      Allergen = Allergy.Lactose | Allergy.Eggs
-                  },
-                  new Dish
-                  {
-                      DishId = 13,
-                      DishName = "Chocolate truffle",
-                      Price = 45,
-                      Description = "(E,G,L,N)",
-                      IsPopular = false,
-                      Category = DishCategories.Dessert,
-                      Allergen = Allergy.Lactose | Allergy.Eggs | Allergy.Gluten | Allergy.Nuts
-                  }
-
-            );
-        }
+      modelBuilder.Entity<Admin>().HasData(
+          new Admin { AdminId = 1, Username = "admin", PasswordHash = "Admin" }
+      );
     }
+    public static void SeedUsers(this ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<User>().HasData(
+          new User { UserId = 1, Name = "Test One", Email = "One@email.com", Phone = "123-456-7890" },
+          new User { UserId = 2, Name = "Test Two", Email = "Two@email.com", Phone = "234-567-8901" },
+          new User { UserId = 3, Name = "Test Three", Email = "Three@email.com", Phone = "345-678-9012" }
+      );
+    }
+    public static void SeedTables(this ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Table>().HasData(
+          new Table { TableId = 1, TableNum = 101, Capacity = 2 },
+          new Table { TableId = 2, TableNum = 102, Capacity = 2 },
+          new Table { TableId = 3, TableNum = 201, Capacity = 4 },
+          new Table { TableId = 4, TableNum = 202, Capacity = 4 },
+          new Table { TableId = 5, TableNum = 203, Capacity = 4 },
+          new Table { TableId = 6, TableNum = 204, Capacity = 4 },
+          new Table { TableId = 7, TableNum = 301, Capacity = 8 }
+      );
+    }
+
+    public static void SeedDishes(this ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Dish>().HasData(
+          new Dish
+          {
+            DishId = 1,
+            DishName = "Skagen Sandwich",
+            Price = 175,
+            Description = "sourdough bread, pickled cucumber trout roe(E,G,L)",
+            IsPopular = true,
+            Category = DishCategories.Starters,
+            Allergen = Allergy.Gluten | Allergy.Eggs | Allergy.Lactose,
+            ImageUrl = "https://shared.cdn.smp.schibsted.com/v2/images/4c0c27ff-0d8d-4616-91a4-5ade5020e187?fit=crop&h=1426&w=1900&s=c946556f4ea073d8d4bcbd9f68761e90c8be64f0"
+          },
+          new Dish
+          {
+            DishId = 2,
+            DishName = "Skagen Sandwich",
+            Price = 245,
+            Description = "sourdough bread, pickled cucumber trout roe (E,G,L)",
+            IsPopular = false,
+            Category = DishCategories.Starters,
+            Allergen = Allergy.Gluten | Allergy.Eggs | Allergy.Lactose
+          },
+          new Dish
+          {
+            DishId = 3,
+            DishName = "Chantarelle toast",
+            Price = 160,
+            Description = "parmesan grilled sour dough (G,L)",
+            IsPopular = true,
+            Category = DishCategories.Starters,
+            Allergen = Allergy.Gluten | Allergy.Lactose,
+            ImageUrl = "https://i.ytimg.com/vi/-cK2rs86oSQ/hqdefault.jpg"
+          },
+          new Dish
+          {
+            DishId = 4,
+            DishName = "Smoked Tiger shrimps",
+            Price = 175,
+            Description = "pickled red onions, new potatoes (L)",
+            IsPopular = false,
+            Category = DishCategories.Starters,
+            Allergen = Allergy.Lactose
+          },
+          new Dish
+          {
+            DishId = 5,
+            DishName = "Steak Tartare",
+            Price = 185,
+            Description = "angel fries, parmesan, grilled heart salad",
+            IsPopular = false,
+            Category = DishCategories.Starters
+          },
+          new Dish
+          {
+            DishId = 6,
+            DishName = "Nicoise",
+            Price = 275,
+            Description = "tuna, egg, capers",
+            IsPopular = false,
+            Category = DishCategories.Salad,
+            Allergen = Allergy.Eggs
+          },
+          new Dish
+          {
+            DishId = 7,
+            DishName = "Moules frites",
+            Price = 285,
+            Description = "aioli (E,L)",
+            IsPopular = false,
+            Category = DishCategories.Mains,
+            Allergen = Allergy.Lactose | Allergy.Eggs
+          },
+            new Dish
+            {
+              DishId = 8,
+              DishName = "Baked Trout",
+              Price = 295,
+              Description = "Sandefjord sauce, trout roe, Chantarelles, boiled potatoes (L)",
+              IsPopular = true,
+              Category = DishCategories.Mains,
+              Allergen = Allergy.Lactose,
+              ImageUrl = "https://www.livinglou.com/wp-content/uploads/2010/08/oven-baked-trout-fillets.jpg"
+            },
+            new Dish
+            {
+              DishId = 9,
+              DishName = "Ossobuco",
+              Price = 295,
+              Description = "veal, crushed potatoes, red wine sauce, creamy gremolata (E,G,L)",
+              IsPopular = false,
+              Category = DishCategories.Mains,
+              Allergen = Allergy.Lactose | Allergy.Gluten | Allergy.Eggs
+            },
+            new Dish
+            {
+              DishId = 10,
+              DishName = "Pepper Steak",
+              Price = 395,
+              Description = "green peppar sauce, French fries, parmesan (L)",
+              IsPopular = true,
+              Category = DishCategories.Mains,
+              Allergen = Allergy.Lactose,
+              ImageUrl = "https://embed.widencdn.net/img/beef/mmnbci53nd/1540x1284px/t-bone-steaks-&-grilled-vegetables-vertical.eps?keep=c"
+            },
+            new Dish
+            {
+              DishId = 11,
+              DishName = "Apple pie",
+              Price = 130,
+              Description = "vanilla ice cream (E,L)",
+              IsPopular = true,
+              Category = DishCategories.Dessert,
+              Allergen = Allergy.Lactose | Allergy.Gluten | Allergy.Eggs,
+              ImageUrl = "https://onedishkitchen.com/wp-content/uploads/2018/10/apple-pie-one-dish-kitchen-1-1.jpg"
+            },
+            new Dish
+            {
+              DishId = 12,
+              DishName = "Creme brulee",
+              Price = 110,
+              Description = "(E,L)",
+              IsPopular = false,
+              Category = DishCategories.Dessert,
+              Allergen = Allergy.Lactose | Allergy.Eggs
+            },
+            new Dish
+            {
+              DishId = 13,
+              DishName = "Chocolate truffle",
+              Price = 45,
+              Description = "(E,G,L,N)",
+              IsPopular = false,
+              Category = DishCategories.Dessert,
+              Allergen = Allergy.Lactose | Allergy.Eggs | Allergy.Gluten | Allergy.Nuts
+            }
+
+      );
+    }
+  }
 }
